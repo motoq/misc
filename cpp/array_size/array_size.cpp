89c89a8221d8d7d2d3f3d38436892a4a57dd5998
Kurt Motekew <motekew@verizon.net>
2020-03-11 23:10:51 -0400

    added enum indexing util

--- cpp/array_size/array_size.cpp
+++ cpp/array_size/array_size.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <tuple>
 
 #include <utl_smeyers.h>
 
@@ -17,6 +18,7 @@ int main()
    * standard C++ array at compile time.  Note the array itself does not
    * need to be constexpr.  How cool is that...
    */
+  std::cout << "\n*\n* Demonstrating compile time array size evaluation\n*\n";
   constexpr double coeffs[] = { 1., 2., 3., 4., 5., 6., 7., 8., 9. };
   constexpr int ncoeff = arraySize(coeffs);
   std::cout << "\nDouble array size is " << ncoeff << " - very cool!";
@@ -39,6 +41,27 @@ int main()
   print_int_array(a);
   std::cout << '\n';
 
+
+
+  std::cout << "\n\n*\n* Demonstrating compile time enum class indexing\n*\n";
+  enum Ndx { first, second }; 
+  enum class NdxC { first, second }; 
+  auto tvals = std::tuple<int, int>{-1, 1};
+
+  std::cout << "\nIndexing a Tuple with an enum";
+  std::cout << "\n\tFirst: " << std::get<first>(tvals);
+  std::cout << "    Second: " << std::get<second>(tvals);
+  
+  std::cout << "\nIndexing a Tuple with an enum class C++14 style";
+  std::cout << "\n\tFirst: " << std::get<endx(NdxC::first)>(tvals);
+  std::cout << "    Second: " << std::get<endx(NdxC::second)>(tvals);
+  
+  std::cout << "\nIndexing a Tuple with an enum class C++11 style";
+  std::cout << "\n\tFirst: " << std::get<endx_(NdxC::first)>(tvals);
+  std::cout << "    Second: " << std::get<endx_(NdxC::second)>(tvals);
+  
+
+  std::cout << '\n';
 }
 
 
--- cpp/array_size/utl_smeyers.h
+++ cpp/array_size/utl_smeyers.h
@@ -1,5 +1,10 @@
-#include <cstddef>
 #include <iostream>
+
+// arraySize
+#include <cstddef>
+
+// toUType
+#include <type_traits>
 
 /**
  * The arraySize utility function from "Effective Modern C++ by Scott Meyers
@@ -26,5 +31,45 @@ void print_int_array(int (&vals)[array_size]) {
   for (int it : vals) {
     std::cout << it << "  ";
   }
+}
+
+
+/**
+ * The toUType utility function from "Effective Modern C++ by Scott Meyers
+ * (O'Reilly).  Copyright 2015 Scott Meyers, 978-1-491-90399-5."
+ *
+ * A C++14 compatible compile time evaluation of a class enum component
+ * to its underlying type.  Useful for providing array or container
+ * indexing by name vs. hard coded numbers while avoiding namespace
+ * pollution that comes with traditional unscoped enums.
+ *
+ * @return  An enum component (enumerator) value by the underlying enumeration
+ * type.
+ */
+template<typename E>
+constexpr auto endx(E enumerator) noexcept
+{
+  return static_cast<std::underlying_type_t<E>>(enumerator);
+}
+
+
+/**
+ * The toUType utility function from "Effective Modern C++ by Scott Meyers
+ * (O'Reilly).  Copyright 2015 Scott Meyers, 978-1-491-90399-5."
+ *
+ * A C++11 compatible compile time evaluation of a class enum component
+ * to its underlying type.  Useful for providing array or container
+ * indexing by name vs. hard coded numbers while avoiding namespace
+ * pollution that comes with traditional unscoped enums.
+ *
+ * @return  An enum component (enumerator) value by the underlying enumeration
+ * type.
+ */
+template<typename E>
+constexpr typename std::underlying_type<E>::type
+   endx_(E enumerator) noexcept
+{
+  return static_cast<typename
+                     std::underlying_type<E>::type>(enumerator);
 }
 
