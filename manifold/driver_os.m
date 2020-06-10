ae310e8b0bbae49bda0c49d22d0f3a44717e2706
Kurt Motekew <motekew@verizon.net>
2020-06-09 23:05:59 -0400

    Minor os plot updates

--- manifold/driver_os.m
+++ manifold/driver_os.m
@@ -23,6 +23,8 @@ fprintf(' %i points is %1.2e meters', npts, 6378137*(dxyz/a));
 p_lambdas = -pi:30*pi/180:pi;
 p_etas = -.9:.15:.9;
 p_npts = size(p_lambdas, 2);
+
+  % Plot covariant basis vectors
 for ii = 1:p_npts
   xyz = mth_os2cart(e, a, p_lambdas(ii), p_etas(ii));
   scatter3(xyz(1,:), xyz(2,:), xyz(3,:), 55, .9, 'filled');
@@ -33,6 +35,7 @@ stitle = sprintf('Oblate Spheroid (e = %1.2f) and %s',...
 title(stitle);
 axis equal;
 
+  % Plot contravariant basis vectors
 plt_os(e, a, lambdas, etas, 0);
 for ii = 1:p_npts
   xyz = mth_os2cart(e, a, p_lambdas(ii), p_etas(ii));
@@ -44,6 +47,8 @@ stitle = sprintf('Oblate Spheroid (e = %1.2f) and %s',...
 title(stitle);
 axis equal;
 
+  % Alternate mapping covariante and contravariant metric tensors to
+  % Cartesian space and plot
 plt_os(e, a, lambdas, etas, 0);
 for ii = 1:p_npts
   xyz = mth_os2cart(e, a, p_lambdas(ii), p_etas(ii));
@@ -57,16 +62,18 @@ for ii = 1:p_npts
   end
   surf(XX + xyz(1), YY + xyz(2), ZZ + xyz(3));
 end
-stitle = sprintf('Oblate Spheroid (e = %1.2f), Transformed Metric Tensor', e);
+stitle = sprintf('Oblate Spheroid (e = %1.2f), Transformed Metric Tensors', e);
 title(stitle);
 axis equal;
 
+  % Choose  specific point to look at
 lambda = lambdas(18);
 eta = etas(18);
+fprintf('\n\nChoosing lambda = %1.1f deg and eta = %1.3f', 180*lambda/pi, eta);
 [e_1, e_2, e_3] = mth_os_cov_basis(e, a, lambda, eta);
 [e1, e2, e3] = mth_os_cont_basis(e, a, lambda, eta);
-fprintf("\n\ndot(ei, e_i) should be 1: %1.3f, %1.3f, %1.3f",...
-                      dot(e1,e_1), dot(e2,e_2), dot(e3,e_3));
+fprintf("\ndot(ei, e_i) should be 1: %1.3f, %1.3f, %1.3f",...
+                    dot(e1,e_1), dot(e2,e_2), dot(e3,e_3));
 fprintf(...
   "\ndot(ei, e_j), should be 0: %1.3f, %1.3f, %1.3f %1.3f, %1.3f %1.3f",...
                                   dot(e1,e_2), dot(e1,e_3), dot(e2,e_3),...
@@ -77,21 +84,29 @@ fprintf("\nNorm of contravariant basis vectors %1.3f %1.3f, %1.3f",...
                                                 norm(e1), norm(e2), norm(e3));
 g_ij = mth_os_mt_cov(e, a, eta);
 gij = mth_os_mt_cont(e, a, eta);
-fprintf("\ng_ij*gij - I = %1.3f", norm(norm(g_ij*gij - eye(3))));
+fprintf("\ng_ij*gij - I = %1.3e", norm(norm(g_ij*gij - eye(3))));
 
-g_ij = mth_os_mt_cov(e, a, eta);
 figure;  hold on;
+  % Plot covariant metric tensor
+g_ij = mth_os_mt_cov(e, a, eta);
 matrix3X3_plot(g_ij, 40, false);
+  % Plot contravariant metric tensor
+gij = mth_os_mt_cont(e, a, eta);
+matrix3X3_plot(gij, 40, false);
+  % Map contravariant metric tensor to Cartesian and plot
 J = mth_dcart_dos(e, a, lambda, eta);
+gij = J*gij*J';
+matrix3X3_plot(gij, 40, true);
+  % Map covariant metric tensor to Cartesian and plot
 g_ij = (J^-1)'*g_ij*(J^-1);
-matrix3X3_plot(g_ij, 40, true);
+matrix3X3_plot(g_ij, 40, false);
 xlabel('x');
 ylabel('y');
 zlabel('z');
-title('Covariant Oblate Spheroid and Transformed Metric Tensors');
+title('Covariant, Contravariant, and Transformed Metric Tensors');
 axis equal;
 
-fprintf("\nJ'*gij*J - I = %1.3f", norm(norm(g_ij - eye(3))));
+fprintf("\nJ'*gij*J - I = %1.3e", norm(norm(g_ij - eye(3))));
 
 
 fprintf('\n');
