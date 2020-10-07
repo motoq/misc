c636765d94218ca03074caa5786b54c9e65e3f6f
Kurt Motekew <motekew@verizon.net>
2020-10-06 23:57:02 -0400

    added accessors

--- cpp/tmp_class/tmp_class.cpp
+++ cpp/tmp_class/tmp_class.cpp
@@ -10,6 +10,9 @@ public:
 
   std::array<TYPE, DIM> getArray() const;
 
+  TYPE& operator[](std::size_t ndx);
+  TYPE operator[](std::size_t ndx) const;
+
 private:
   std::array<TYPE, DIM> a;
 };
@@ -19,7 +22,7 @@ template <typename TYPE, std::size_t DIM>
 StdArrayTest<TYPE, DIM>::StdArrayTest()
 {
   TYPE def_val = static_cast<TYPE>(0);
-  for (std::size_t ii=static_cast<TYPE>(0); ii<DIM; ++ii) {
+  for (std::size_t ii=static_cast<std::size_t>(0); ii<DIM; ++ii) {
     a[ii] = def_val;
   }
 }
@@ -27,7 +30,7 @@ StdArrayTest<TYPE, DIM>::StdArrayTest()
 template <typename TYPE, std::size_t DIM>
 StdArrayTest<TYPE, DIM>::StdArrayTest(TYPE def_val)
 {
-  for (std::size_t ii=static_cast<TYPE>(0); ii<DIM; ++ii) {
+  for (std::size_t ii=static_cast<size_t>(0); ii<DIM; ++ii) {
     a[ii] = def_val;
   }
 }
@@ -39,13 +42,56 @@ std::array<TYPE, DIM> StdArrayTest<TYPE, DIM>::getArray() const
 }
 
 template <typename TYPE, std::size_t DIM>
+TYPE& StdArrayTest<TYPE, DIM>::operator[](std::size_t ndx)
+{
+  return a[ndx];
+}
+
+template <typename TYPE, std::size_t DIM>
+TYPE StdArrayTest<TYPE, DIM>::operator[](std::size_t ndx) const
+{
+  return a[ndx];
+}
+
+template <typename TYPE, std::size_t DIM>
 void printArray(const std::array<TYPE, DIM>& a2p)
 {
   std::cout << '\n';
-  for (std::size_t ii=static_cast<TYPE>(0); ii<DIM; ++ii) {
+  for (std::size_t ii=static_cast<std::size_t>(0); ii<DIM; ++ii) {
     std::cout << ' ' << a2p[ii];
   }
 }
+
+template <typename TYPE, std::size_t DIM, std::size_t DIM_SLICE>
+StdArrayTest<TYPE, DIM_SLICE>
+slice(const StdArrayTest<TYPE, DIM>& sat1, std::size_t offset)
+{
+  StdArrayTest<TYPE, DIM_SLICE> sat2;
+
+  for (std::size_t ii=static_cast<std::size_t>(0); ii<DIM_SLICE; ++ii) {
+    sat2[ii] = sat1[ii+offset];
+  }
+
+  return sat2;
+}
+
+template <typename TYPE, std::size_t DIM1, std::size_t DIM2>
+StdArrayTest<TYPE, DIM1+DIM2>
+cat(const StdArrayTest<TYPE, DIM1>& sat1,
+    const StdArrayTest<TYPE, DIM2>& sat2)
+{
+  StdArrayTest<TYPE, DIM1 + DIM2> sat3;
+
+  for (std::size_t ii=static_cast<std::size_t>(0); ii<DIM1; ++ii) {
+    sat3[ii] = sat1[ii];
+  }
+  for (std::size_t ii=static_cast<std::size_t>(0); ii<DIM2; ++ii) {
+    sat3[ii+DIM1] = sat2[ii];
+  }
+
+  return sat3;
+}
+
 
 /**
  *
@@ -54,6 +100,7 @@ void printArray(const std::array<TYPE, DIM>& a2p)
 int main()
 {
   constexpr std::size_t n {3};
+  constexpr std::size_t n2 {n - 1};
 
   StdArrayTest<double, n> sat0;
   auto a0 = sat0.getArray();
@@ -62,6 +109,16 @@ int main()
   StdArrayTest<double, n> sat1(1.0);
   auto a1 = sat1.getArray();
   printArray(a1);
+
+  std::cout << "\nsat1[1]: " << sat1[1];
+  sat1[1] = 3.0;
+  std::cout << "\nChanged sat1[1]: " << sat1[1];
+
+  StdArrayTest<double, n2> sat2 = slice<double, n, n2>(sat1, 1);
+  printArray(sat2.getArray());
+
+  StdArrayTest<double, n+n2> sat3 = cat<double, n, n2>(sat1, sat2);
+  printArray(sat3.getArray());
 
   std::cout << '\n';
 }
