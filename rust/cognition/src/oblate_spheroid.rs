14fcad6317a0b011f2dc2c0f37140488b99a61b3
Kurt Motekew <motekew@verizon.net>
2024-09-06 01:28:01 -0400

    def init variables

--- rust/cognition/src/oblate_spheroid.rs
+++ rust/cognition/src/oblate_spheroid.rs
@@ -1,20 +1,20 @@
 
 #[derive(Default)]
 pub struct OblateSpheroid {
-    m_eccen: f64,
-    m_smajor: f64,
-    m_lambda: f64,
-    m_eta: f64,
+    eccen: f64,
+    smajor: f64,
+    lambda: f64,
+    eta: f64,
 }
 
 
 impl OblateSpheroid {
     pub fn new(eccen: f64, smajor: f64, lambda: f64, eta: f64) -> Self {
         Self {
-            m_eccen: eccen,
-            m_smajor: smajor,
-            m_lambda: lambda,
-            m_eta: eta,
+            eccen,
+            smajor,
+            lambda,
+            eta,
         }
     }
 }
@@ -25,7 +25,7 @@ impl std::fmt::Display for OblateSpheroid {
         const DPR: f64 = 180.0/std::f64::consts::PI;
         write!(f, "(\n  Eccentricity: {};  Semimajor: {}\
                    ;  Lambda: {};  Eta: {})",
-            self.m_eccen, self.m_smajor, DPR*self.m_lambda, self.m_eta)
+            self.eccen, self.smajor, DPR*self.lambda, self.eta)
     }
 }
 
