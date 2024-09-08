de54c343c8303650e71d035c54a0e44a6d64e193
Kurt Motekew <motekew@verizon.net>
2024-09-08 01:45:34 -0400

    updated input checker

--- rust/cognition/src/oblate_spheroid.rs
+++ rust/cognition/src/oblate_spheroid.rs
@@ -1,10 +1,10 @@
 
-#[derive(Default)]
 pub struct OblateSpheroid {
     eccen: f64,
     smajor: f64,
     lambda: f64,
     eta: f64,
+    cart: [f64; 3],
 }
 
 
@@ -12,20 +12,45 @@ impl OblateSpheroid {
     pub fn new(eccen: f64,
                smajor: f64,
                lambda: f64,
-               eta: f64) -> Result<Self, String> {
+               eta: f64) -> Result<OblateSpheroid, String> {
         if eccen < 0.0   ||  eccen >= 1.0 {
             return Err("Invalid Eccentricity".to_string());
+        } else if smajor < 0.0 {
+            return Err("Invalid Semimajor Axis".to_string());
+        } else if lambda < -0.5*std::f64::consts::PI  ||
+                  lambda >  0.5*std::f64::consts::PI {
+            return Err("Invalid Azimuth".to_string());
+        } else if eta < -1.0  ||  eta >  1.0 {
+            return Err("Invalid Elevation".to_string());
         }
-        Ok(Self {
-                eccen,
-                smajor,
-                lambda,
-                eta,
-        })
+        let mut os = OblateSpheroid {eccen,
+                                     smajor,
+                                     lambda,
+                                     eta,
+                                     cart: Default::default(),
+        };
+        os.set_with_os(eccen, smajor, lambda, eta);
+        Ok(os)
     }
 
-    pub fn new2() -> Self {
-        Default::default()
+    fn set_with_os(&mut self, ecc: f64, sma: f64, lam: f64, eta: f64) {
+        let sqometa2 = (1.0 - eta*eta).sqrt();
+
+        self.cart[0] = sma*sqometa2*lam.cos();
+        self.cart[1] = sma*sqometa2*lam.sin();
+        self.cart[2] = sma*eta*(1.0 - ecc*ecc).sqrt();
+    }
+}
+
+impl Default for OblateSpheroid {
+    fn default() -> Self {
+        Self {
+            eccen: 0.0,
+            smajor: 0.0,
+            lambda: 0.0,
+            eta: 0.0,
+            cart: [0.0, 0.0, 0.0],
+        }
     }
 }
 
--- rust/oblsph/src/main.rs
+++ rust/oblsph/src/main.rs
@@ -6,30 +6,24 @@ fn main() {
 
     const RAD_PER_DEG: f64 = std::f64::consts::PI/180.0;
 
-    let max_smaj = f64::sqrt(0.1*std::f64::MAX);
-
+    // Get Inputs
     println!("Enter the eccentricity (0 <= e < 1):");
-    let eccen: f64 = obls_read_input(0.0, 0.9);
-
+    let eccen: f64 = read_input();
     println!("Enter the semimajor axis length (0 <= a):");
-    let smajor: f64 = obls_read_input(0.0, max_smaj);
-
+    let smajor: f64 = read_input();
     println!("Enter longitude (-180 <= lam <= 180):");
-    let lambda: f64  = RAD_PER_DEG*obls_read_input(-180.0, 180.0);
-
+    let lambda: f64  = RAD_PER_DEG*read_input();
     println!("Enter elevation (-1 <= eta <= 1):");
-    let eta: f64 = obls_read_input(-1.0, 1.0);
+    let eta: f64 = read_input();
 
     let os = if let Ok(os) =
         oblate_spheroid::OblateSpheroid::new(eccen, smajor, lambda, eta) {
         os
     } else {
+        println!("Bad OblateSpheroid Init");
         return;
     };
 
-    println!("OblateSpheroid {}", os);
-
-    let os = oblate_spheroid::OblateSpheroid::new2();
     println!("OblateSpheroid {}", os);
 
     let os: oblate_spheroid::OblateSpheroid  = Default::default();
@@ -39,7 +33,24 @@ fn main() {
 }
 
 
-/**
+fn read_input() -> f64 {
+    let ret_val = loop {
+        let mut str_val = String::new();
+        io::stdin()
+            .read_line(&mut str_val)
+            .expect("Failed to read input from command line!");
+        let val = match str_val.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        break val;
+    };
+
+    ret_val
+}
+
+
+/*
  * Requests user input from the CL.  A single f64 with the given limits
  * is returned.  The function will continue to ask for a value until a
  * valid one is entered.
@@ -48,7 +59,6 @@ fn main() {
  * @param  max_val  Maximum allowable value
  *
  * @return  Parsed value meeting requirements
- */
 fn obls_read_input(min_val: f64, max_val: f64) -> f64 {
 
     let mut bad_val = 2.0*max_val;
@@ -73,5 +83,6 @@ fn obls_read_input(min_val: f64, max_val: f64) -> f64 {
 
     ret_val
 }
+ */
 
 
