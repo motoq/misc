4d6e37998cd8789344dc97b291eba2668bbba9a1
Kurt Motekew <motekew@verizon.net>
2024-11-24 19:58:33 -0500

    plot writer to buffered

--- cognition/oblsph/src/plot_os.rs
+++ cognition/oblsph/src/plot_os.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::Write;
+use std::io::{Write, BufWriter};
 
 use crate::Config;
 use cogs::oblate_spheroid;
@@ -11,18 +11,21 @@ pub fn plot_os(os: &oblate_spheroid::OblateSpheroid,
 
     let mut file_name = cfg.plot_prefix.clone();
     file_name.push_str(".gp");
-    let mut output = File::create(&file_name)?;
-    write!(output, "set title \"Oblate Spheroid\"")?;
-    write!(output, "\nset parametric")?;
-    write!(output, "\nset isosamples 25")?;
-    write!(output, "\nsplot [-pi:pi][-pi/2:pi/2]")?;
-    write!(output, " {:.3e}*cos(u)*cos(v)", os.get_semimajor())?;
-    write!(output, ", {:.3e}*sin(u)*cos(v)", os.get_semimajor())?;
-    write!(output, ", {:.3e}*sin(v)", os.get_semiminor())?;
-    write!(output, "\nset view equal xyz")?;
+    let file = File::create(&file_name)?;
+    let mut writer = BufWriter::new(file);
+    write!(writer, "set title \"Oblate Spheroid\"")?;
+    write!(writer, "\nset parametric")?;
+    write!(writer, "\nset isosamples 25")?;
+    write!(writer, "\nsplot [-pi:pi][-pi/2:pi/2]")?;
+    write!(writer, " {:.3e}*cos(u)*cos(v)", os.get_semimajor())?;
+    write!(writer, ", {:.3e}*sin(u)*cos(v)", os.get_semimajor())?;
+    write!(writer, ", {:.3e}*sin(v)", os.get_semiminor())?;
+    write!(writer, "\nset view equal xyz")?;
 
 
-    //write!(output, "\npause mouse close\n")
+    //write!(writer, "\npause mouse close\n")
+
+    writer.flush()?;
     Ok(())
 }
 
