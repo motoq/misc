a5ca6e9e6310e2d12b0762a38f0b731de344e392
Kurt Motekew <motekew@verizon.net>
2024-12-27 01:54:20 -0500

    switched to Arc

--- rust/thread_share/src/main.rs
+++ rust/thread_share/src/main.rs
@@ -1,29 +1,28 @@
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 use std::thread;
 
+/**
+ * Demonstrates sharing of the same memory across threads using a
+ * smart pointer type that does not support mutable dereferencing.
+ * The underlying type is mutable.  It is moved into an Arc, which
+ * is then cloned for each thread.
+ */
 fn main() {
-    println!("Hello, world!");
-
-    // Example from book
-    let v = vec![1, 2, 3];
-    let handle = thread::spawn(move || {
-        println!("Here's a vector: {v:?}");
-    });
-    handle.join().unwrap();
-
-    // Shared memory across threads example
-    let p0 = Point::default();
+    // Work with actual point
+    let mut p0 = Point::default();
+    p0.x = 7.5;
+    p0.y = 5.0;
     println!("Point {}", p0);
 
-    // Pointer to point
-    let pp0 = Arc::new(Mutex::new(p0));
-    println!("Point pointer {}", pp0.lock().unwrap());
-
+    // Move p0 into read only pointer to point
+    // immutable Dref only via Arc
+    let pp0 = Arc::new(p0);
+    println!("Point pointer {}", pp0);
     let mut handles = vec![];
     for _ in 0..10 {
         let pp0 = Arc::clone(&pp0);
         let handle = thread::spawn(move || {
-            println!("Thread point pointer {}", pp0.lock().unwrap());
+            println!("Thread point pointer {}", pp0);
         });
         handles.push(handle);
     }
@@ -33,15 +32,15 @@ fn main() {
 }
 
 struct Point {
-    pub x : f64,
+    pub x: f64,
     pub y: f64,
 }
 
 impl Default for Point {
     fn default() -> Self {
         Self {
-            x : 0.0,
-            y : 0.0,
+            x: 0.0,
+            y: 0.0,
         }
     }
 }
