4130be77acdf5c18e0d18c9f029575ffbc0d37e2
Kurt Motekew <motekew@verizon.net>
2025-12-03 00:09:48 -0500

    updating Eigen frame xform example to be more clear

--- cpp/eigen_mqrot/eigen_mqrot.cpp
+++ cpp/eigen_mqrot/eigen_mqrot.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <numbers>
 
 #define EIGEN_NO_MALLOC
 #include <Eigen/Dense>
@@ -14,24 +15,47 @@ int main()
   using namespace std;
   using namespace Eigen;
 
-    // Ground truth RPY matrix reference frame transformation matrix
+    // Aerospace sequence is Yaw   about the z-axis,
+    //                       Pitch about the y-axis, and
+    //                       Roll  about the z-axis
+  constexpr double ax {numbers::pi/5.0};    // Roll
+  constexpr double ay {numbers::pi/4.0};    // Pitch
+  constexpr double az {numbers::pi/3.0};    // Yaw
+
+    // Externally generated ground truth RPY matrix
+    // reference frame transformation matrix
   Matrix3d m_gt;
   m_gt <<  0.35355,  0.61237, -0.70711,
           -0.49282,  0.76445,  0.41563,
            0.79507,  0.20153,  0.57206;
 
-    // Must use negative angle for reference frame transformation
-  Matrix3d rmat {AngleAxisd(-M_PI/5.0, Vector3d::UnitX())};
-  Matrix3d pmat {AngleAxisd(-M_PI/4.0, Vector3d::UnitY())};
-  Matrix3d ymat {AngleAxisd(-M_PI/3.0, Vector3d::UnitZ())};
-  Quaterniond rquat {AngleAxisd(-M_PI/5.0, Vector3d::UnitX())};
-  Quaterniond pquat {AngleAxisd(-M_PI/4.0, Vector3d::UnitY())};
-  Quaterniond yquat {AngleAxisd(-M_PI/3.0, Vector3d::UnitZ())};
+    // Create reference frame transformation matrices manually.
+  Matrix3d rmat;
+  Matrix3d pmat;
+  Matrix3d ymat;
+  rmat << 1.0,      0.0,     0.0,
+          0.0,  cos(ax), sin(ax),
+          0.0, -sin(ax), cos(ax);
+  pmat << cos(ay),  0.0,  -sin(ay),
+              0.0,  1.0,       0.0,
+          sin(ay),  0.0,   cos(ay);
+  ymat <<  cos(az), sin(az), 0.0,
+          -sin(az), cos(az), 0.0,
+           0.0,         0.0, 1.0;
+    // Create Quaternion
+  Quaterniond rquat {AngleAxisd(ax, Vector3d::UnitX())};
+  Quaterniond pquat {AngleAxisd(ay, Vector3d::UnitY())};
+  Quaterniond yquat {AngleAxisd(az, Vector3d::UnitZ())};
 
-    // Combined rotation sequence - standard matrix order of operation
-    // Roll, then pitch, then yaw
+    // Yaw, then pitch, then roll
+    //
+    // For the matrix library, multipley reference frame
+    // transformations right to left
   const Matrix3d m {rmat*pmat*ymat};
-  const Quaterniond q {rquat*pquat*yquat};
+    // For quaternion rotation sequences, multiply left to right,
+    // then take the inverse, to get the equivalent reference frame
+    // transformation.
+  const Quaterniond q {(yquat*pquat*rquat).conjugate()};
 
     // Test vector
   const Vector3d x {1.0, 1.0, 1.0};
@@ -39,15 +63,15 @@ int main()
   const Vector3d qx {q*x};
   const Vector3d dx {mx - qx};
 
-  std::cout << "\n\nExpected m:\n";
-  std::cout << m_gt;
-  std::cout << "\nComputed m:\n";
-  std::cout << m;
-  std::cout << "\nmx:\n";
-  std::cout << m*x;
-  std::cout << "\nqx:\n";
-  std::cout << q*x;
-  std::cout << "\nDelta: " << dx.norm();
+  cout << "\n\nExpected m:\n";
+  cout << m_gt;
+  cout << "\nComputed m:\n";
+  cout << m;
+  cout << "\nmx:\n";
+  cout << mx;
+  cout << "\nqx:\n";
+  cout << qx;
+  cout << "\nDelta: " << dx.norm();
 
   cout << '\n';
 }
