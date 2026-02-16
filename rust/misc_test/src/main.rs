6e440a69c0a6bf52a437706a4043e411f39dc40e
Kurt Motekew <motekew@verizon.net>
2026-02-15 21:20:21 -0500

    updated Copy/Clone example

--- rust/misc_test/src/main.rs
+++ rust/misc_test/src/main.rs
@@ -1,11 +1,14 @@
 /// List summary of experiments
 fn main() {
-    println!("\n  Pattern matching experiments:");
-    println!("$ cargo run --bin patterns");
-
-    println!("\n  Generic float and to a lesser extent int variable");
-    println!("    experiments for creating math and physics libraries:");
+    println!("\n    # Generic float and to a lesser extent int variable");
+    println!("    # experiments for creating math and physics libraries:");
     println!("$ cargo run --bin generic_num");
+
+    println!("\n    # Implicity and explicity Copy and clone:");
+    println!("$ cargo run --bin move");
+
+    println!("\n    # Pattern matching experiments:");
+    println!("$ cargo run --bin patterns");
 
     println!("");
 }
--- rust/misc_test/src/move/main.rs
+++ rust/misc_test/src/move/main.rs
@@ -1,5 +1,7 @@
 /// Demonstraates implicity copy of POD when Copy trait is
-/// associated with a structure.
+/// associated with a structure, along with explicit Clone.
+///
+/// 2026/02/15
 
 /// Default structure with POD
 struct Point {
@@ -7,7 +9,15 @@ struct Point {
     y: f64,
 }
 
-/// ...with Copy
+/// ...with Clone only
+///
+#[derive(Clone)]
+struct PointClone {
+    x: f64,
+    y: f64,
+}
+
+/// ...with Copy (and Clone)
 /// Note, Copy is a marker (empty) trait does have any method and
 /// instead indicates properties/behaviors of the structure
 ///  Hence, .copy() is not called, but .clone(), if used explicity. 
@@ -20,6 +30,10 @@ struct PointCopy {
 
 fn main() {
 
+    //
+    // Default behavior, move
+    //
+
     let p1 = Point {
         x: 7.5,
         y: 5.0,
@@ -28,19 +42,44 @@ fn main() {
     // Copy not implemented, p2 takes possesion of p1
     let p2 = p1;
 
-    //println!("p1 ({}, {}k)", p1.x, p1.y);  // Nope, p1 invalid
+    //println!("p1 ({}, {}k)", p1.x, p1.y);      // Nope, p1 invalid
     println!("p2 ({}, {}k)", p2.x, p2.y);
+
+    //
+    // Enable explicit clone()
+    //
+
+    println!("");
+
+    let p1 = PointClone {
+        x: 6.0,
+        y: 5.5,
+    };
+
+    // p2 takes possession of p1 as no implicit clone and copy not
+    // enabled
+    let p2 = p1;
+    // Explicit clone()
+    let p3 = p2.clone();
+
+    //println!("p1 ({}, {}k)", p1.x, p1.y);      // Nope, p1 invalid
+    println!("p2 ({}, {}k)", p2.x, p2.y);
+    println!("p3 ({}, {}k)", p3.x, p3.y);
+
+    //
+    // Copy and Clone enabled
+    //
 
     println!("");
 
     let p1 = PointCopy {
         x: 6.0,
-        y: 5.5,
+        y: 5.0,
     };
 
-    // Implicit copy
+    // Implicit copy - copy() not explicitly callable
     let p2 = p1;
-    // Explicit (copy() not callable - use clone())
+    // Explicit via clone
     let p3 = p1.clone();
 
     println!("p1 ({}, {}k)", p1.x, p1.y);
